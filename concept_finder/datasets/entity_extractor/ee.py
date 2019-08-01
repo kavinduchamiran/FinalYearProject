b36182f3d232ddb0daadfcf968b2c7abc60c5c5e
kavinduchamiran <kavindu.chamiran@gmail.com>
2019-08-01 14:10:32 +0530

    ee fixes

--- concept_finder/datasets/entity_extractor/ee.py
+++ concept_finder/datasets/entity_extractor/ee.py
@@ -6,9 +6,13 @@ import random
 import urllib3
 import re
 
+os.remove('label_to_uri.txt')
+os.remove('label_to_uri_failed.txt')
+os.remove('label_to_uri_cleaned.txt')
+
 def special_match(strg, search=re.compile(r'[+a-z]').search):
     # regex at least one a-z
-    return bool(search(strg)) and len(strg) < 20
+    return bool(search(strg)) and len(strg) > 2 and len(strg) < 20
 
 from multiprocessing.dummy import Pool as ThreadPool
 
@@ -26,20 +30,22 @@ def query_dbpedia_lookup_endpoint(x):
 
     filename, col, entity_label = x
 
-    url = 'http://lookup.dbpedia.org/api/search/KeywordSearch?MaxHits=1&QueryString=%s' % entity_label
+    url = 'http://lookup.dbpedia.org/api/search/KeywordSearch?MaxHits=1&QueryString=%s' % entity_label.replace(' ', '%20')
+
     http = urllib3.PoolManager()
 
     req = http.request('GET', url, headers={'Accept': 'application/json'})
+
     if req.status == 200:
         json_data = json.loads(req.data)
 
         if json_data['results']:
             uri = json_data['results'][0]['uri']
             with open('label_to_uri.txt', 'a+') as dest:
-                dest.write("{}\t{}\n".format(entity_label, uri))
+                dest.write("{}\t{}\n".format(entity_label.lower(), uri))
         else:
             with open('label_to_uri_failed.txt', 'a+') as dest:
-                dest.write("{}\t{}\t{}\n".format(filename, col, entity_label))
+                dest.write("{}\t{}\t{}\n".format(filename, col, entity_label.lower()))
 
 
 
@@ -56,6 +62,8 @@ for filename in os.listdir('./tables'):
         del records[key_column_idx]
 
         for idx, col in enumerate(records):
+            if has_header:
+                col = col[1:]
             for i in col:
                 rows.append((filename, idx, i))
             # rows.extend(map(str.lower, col))
@@ -67,7 +75,7 @@ for filename in os.listdir('./tables'):
 
             uri, label, _ = line
             with open('label_to_uri.txt', 'a+') as dest:
-                dest.write("{}\t{}\n".format(label, uri))
+                dest.write("{}\t{}\n".format(label.lower(), uri))
 
     except Exception as e:
         print(e)
@@ -83,3 +91,17 @@ pool.map(query_dbpedia_lookup_endpoint, rows)
 
 pool.close()
 pool.join()
+
+# cleanup
+
+source = open('./label_to_uri.txt').readlines()
+dest = open('label_to_uri_cleaned.txt', 'a+')
+
+source = list(set(source))
+
+for idx, line in enumerate(source):
+    try:
+        a, b = line.split('\t')
+        dest.write("{}\t{}".format(a.lower().replace('&nbsp', ''), b))
+    except:
+        pass
